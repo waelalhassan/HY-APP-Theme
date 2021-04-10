dcd1114305435907dbe139acc49d00f5e0b22548
wael alhassan <waelalhassan.fed@gmail.com>
2021-04-10 23:48:03 +0300

    contact page added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,47 @@
 
 //? start custom carousel [line 92 in index.pug]
-// parent of items
-let parent = document.querySelector(".items-quot"),
+    // parent of items
+let parent = document.querySelector(".items-quot");
     // items
-    item = document.querySelectorAll(".items-quot .item"),
+let item = document.querySelectorAll(".items-quot .item");
     // button prev
-    prev = document.querySelector(".control .prev"),
+let prev = document.querySelector(".control .prev");
     // button next
-    next = document.querySelector(".control .next"),
+let next = document.querySelector(".control .next");
     // counter
-    count = 0,
+let count = 0;
     // number of items
+let numberItems;
+// if parent not return null
+if (!(parent === null)) {
     numberItems = parent.children.length;
-    
+}
+// loop on items
 item.forEach(e => {
     e.style.display = "none";
-    parent.children[0].style.display = "block";
+    if (parent) {
+        parent.children[0].style.display = "block";
+    }
 })
-
-prev.addEventListener("click", prevEle);
-next.addEventListener("click", nextEle);
+// fix addEventListener if null
+if (prev) {
+    prev.addEventListener("click", prevEle);
+}
+if (next) {
+    next.addEventListener("click", nextEle);
+}
 
 function nextEle() {
 count++;
     if (count > numberItems - 1) {
     count = 0
     }
-
     item.forEach((e) => {
         e.style.display = "none";
-        parent.children[count].style.display = "block";
+        // if parent not null
+        if (parent) {
+            parent.children[count].style.display = "block";
+        }
     })
     // add class active to next btn
     next.classList.add("active")
@@ -45,11 +57,27 @@ function prevEle() {
 count--;
     item.forEach((e) => {
         e.style.display = "none";
-        parent.children[count].style.display = "block";
+        // if parent not null
+        if (parent) {
+            parent.children[count].style.display = "block";
+        }
     })
-
     prev.classList.add("active")
     next.classList.remove("active")
 }
-
 //? end custom carousel [line 92 in index.pug]
+
+//? start fix input text and email when blur [line 33 in contact.pug]
+
+// let text = document.querySelector(".get-in-touch input[type='text']");
+// let parentForm = document.querySelector(".get-in-touch");
+
+// text.addEventListener("blur", ()=> {
+//     if (!(text.value === "")) {
+//         parentForm.classList.add("preventSpan");
+//     } else {
+//         parentForm.classList.remove("preventSpan");
+//     }
+// })
+
+//? end fix input text and email when focus [line 33 in contact.pug]
